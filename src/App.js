b8a6af395a44517340684364f58ce1594ded0a16
kaiser <tandatvok16@gmail.com>
2023-07-10 15:57:58 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
-function App() {
+
+function Task({ task }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='TaskName' style={{
+      padding: 5,
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: 'lightblue',
+      color: 'white',
+      margin: 8,
+      borderRadius: 8,
+      border: '2px solid black',
+      width: 500,
+      marginLeft: 'auto',
+      marginRight: 'auto',
+      fontSize: 20,
+      fontWeight: 'bold',
+      
+    }}>
+      {task}
+    </div>
+  );
+}
+
+
+function App() {
+  const [tasks, addTask] = useState([])
+  const [task, setTask] = useState('');
+  console.log(tasks);
+  function handleAddTask(){
+    if(tasks.includes(task)){
+      return
+    }
+    addTask(prev => [...prev, task]);
+  }
+  return (
+    <div className="App" >
+      <h1>Todo List</h1>
+      <input 
+        type="text" 
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+        placeholder="Add a task" 
+      /> 
+      <button 
+        onClick={handleAddTask}
+      >Add</button>
+        {
+          tasks.map((task , index ) => {
+            return (
+            <Task 
+              key = {index}
+              task = {task} 
+            />
+          )})
+        }
+      
     </div>
   );
 }
